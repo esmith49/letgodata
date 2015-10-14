a007cc1baff2ab53827804ddb8bd7f9f7b9e3683
Evan Smith <esmith49@cnm.edu>
2015-10-14 16:33:24 -0600

    adding persona data starting use case information

--- letgodata.php
+++ letgodata.php
@@ -13,8 +13,10 @@
 		<h2>buyer Profile</h2>
 		<p>Michael Myers is a hipster recent college grad working at the Comcast tech support line. He makes about $35,000 a year and is a collecter of old computer memorabilia. Michael like old things because he is a hipster and liked them before they were cool so he dosen't mind used items.  Michael works for Comcast so he has plenty of time to browse his phones mobile app while at work. </p>
 		<h2> LetGo Use Case</h2>
-		<p><li>Sellers will upload a picture of the item and a seguested sale price.</li>
+		<p><li>Sellers will upload a picture of the item and a suggested sale price.</li>
 			<li>buyer will search the site for an item</li>
+			<li>Based on the distance and price buyer will click on an item</li>
+			<li>Buyer will land </li>
 			<li>buyers will make an offer</li>
 			<li>Once offer is accepted the seller will confirm by e-mail and ship item</li>
 		</p>
