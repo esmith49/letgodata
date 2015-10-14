52620c515286a19246e65cddb92feee4e1ddb702
Evan Smith <esmith49@cnm.edu>
2015-10-14 15:42:27 -0600

    adding persona data

---
+++ letgodata.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>LetGo Buyers and Sellers Information</title>
+	</head>
+	<body>
+		<h1>LetGo Buyers Persona</h1>
+		<p>somebody between the ages of 18 and 35, people who are moving, bargin hunters that are looking for specific items makes less than $50k a year, likes gently used, uses mobile apps, tech savy.
+
+			Michael Myers is a hipster recent college grad working at the Comcast tech support line. He makes about $35,000 a year and is a collecter of old computer memorabilia. Michael like old things because he is a hipster and liked them before they were cool so he dosen't mind used items.  Michael works for Comsact so he has plenty of time to browse his phones mobile app while at work. </p>
+		<h2> LetGo Use Case</h2>
+		<p>Goals Looking to buy or sell something
+		</p>
+	</body>
+</html>
