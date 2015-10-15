fa4f406d31999e3e52daf516beb3fafe734852da
Evan Smith <esmith49@cnm.edu>
2015-10-15 08:09:35 -0600

    adding persona data starting use case information

--- letgodata.php
+++ letgodata.php
@@ -5,27 +5,42 @@
 		<title>LetGo Buyers and Sellers Information</title>
 	</head>
 	<body>
-		<h1>LetGo Buyers Persona</h1>
-		<p>somebody between the ages of 18 and 35, people who are moving, bargin hunters that are looking for specific items makes less than $50k a year, likes gently used, uses mobile apps, tech savy.</p>
+
+		<h1>LetGo Data</h1>
+
+		<h2>LetGo Buyers Persona</h2>
+
+		<p>Somebody between the ages of 18 and 35, people who are moving, bargin hunters that are looking for specific items makes less than $50k a year, likes gently used, uses mobile apps, tech savy.</p>
 
 		<h2>Goals</h2>
-		<p><li>looking to buy old computer equipment</li></p>
+
+		<ul><li>looking to buy old computer equipment</li></ul>
+
 		<h2>buyer Profile</h2>
+
 		<p>Michael Myers is a hipster recent college grad working at the Comcast tech support line. He makes about $35,000 a year and is a collecter of old computer memorabilia. Michael like old things because he is a hipster and liked them before they were cool so he dosen't mind used items.  Michael works for Comcast so he has plenty of time to browse his phones mobile app while at work. </p>
+
 		<h2>LetGo Use Case for the Seller</h2>
-		<p><li>Seller will upload a photo of the item</li>
+
+		<ul>
+			<li>Seller will upload a photo of the item</li>
 			<li>Seller will enter a Product Title</li>
 			<li>Seller will enter a suggested sale price</li>
 			<li>Seller will enter a brief description of the item</li>
-			<li>Seller will choose a general catagory for the item to appear under</li>
-			<li>seller will click the "sell" button</li></p>
+			<li>Seller will choose a general category for the item to appear under</li>
+			<li>seller will click the "sell" button</li>
+		</ul>
+
 		<h2> LetGo Use Case for The Buyer</h2>
 
-		<p><li>buyer will search the site for an item</li>
+		<ul>
+			<li>buyer will search the site for an item</li>
 			<li>Based on the distance and price buyer will click on an item</li>
-			<li>Buyer will land </li>
-			<li>buyers will make an offer</li>
+			<li>Buyer will land on a product</li>
+			<li>buyers will click on "Make an Offer"</li>
+			<li>Buyer will enter a dollar amount in the offer line</li>
+			<li>Buyer will click on "send"</li>
 			<li>Once offer is accepted the seller will confirm by e-mail and ship item</li>
-		</p>
+		</ul>
 	</body>
 </html>
